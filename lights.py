9099dd2852f8371feebc8a421ce69ccc97367417
Julian Shniter <julianshniter@Julians-MacBook-Pro.local>
2021-11-01 13:45:57 -0400

    made changes

--- lights.py
+++ lights.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.bcm)
-GPIO.setup(17, GPIO.OUT)
-while True:
+button=25
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+while True
     GPIO.output(17, GPIO.HIGH)
     sleep(0.5)
     GPIO.output(17, GPIO.LOW)
