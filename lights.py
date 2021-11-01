8c361cd8107c140666f0f5de178ba693e202bf53
Julian Shniter <julianshniter@Julians-MacBook-Pro.local>
2021-11-01 14:02:13 -0400

    more changes

--- lights.py
+++ lights.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
-GPIO.setmode(GPIO.bcm)
-button=25
-led=17
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-while True:
-    if (GPIO.input(button) == GPIO.HIGH):
-        GPIO.output(led, GPIO.HIGH)
+led = 17
+button1 = 25
+button2 = 5
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+while (True):
+	if (GPIO.input(button1) == GPIO.HIGH and GPIO.input(button2) == GPIO.HIGH):
+	    GPIO.output(led, GPIO.HIGH)
     else:
-        GPIO.output(led, GPIO.LOW)
+	    GPIO.output(led, GPIO.LOW)
+        sleep(0.1)
