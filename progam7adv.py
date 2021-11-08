943eb35768ee96cb9c823429c036a778e44b2d59
Julian Shniter <julianshniter@Julians-MacBook-Pro.local>
2021-11-08 12:57:30 -0500

    added program 7

---
+++ progam7adv.py
@@ -0,0 +1,7 @@
+import numpy as np
+#https://www.geeksforgeeks.org/python-program-for-n-queen-problem-backtracking-3/
+#ill just put this here, who knows how much of it i will actually use
+#and this https://www.codespeedy.com/solve-8-queens-problem-in-python/
+N=8
+board = [['x']*N for _ in range(N)]
+print(board)
