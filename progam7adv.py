90933d7b3aad63cf8798c3bd341700ead792e252
Julian Shniter <julianshniter@Julians-MacBook-Pro.local>
2021-11-10 13:38:05 -0500

    added sonic.py

--- progam7adv.py
+++ progam7adv.py
@@ -13,15 +13,34 @@ print(board)
 #do it row by row
 #https://www.techiedelight.com/print-possible-solutions-n-queens-problem/
 #this is going to check if its possible to do it 
-def if_possible(grid, r, x):
+def is_possible(grid, r, x):
+    #this one needs to return a boolean value 
     #first i got to check the horizontal values
     for i in range(r):
-        if board[i][x]=='Q': 
+        if board[i][x]=='Q':
             return False
     #then check diagonal values
-            
+    #we need to iterate over the previous top rows and cokumns to check if a queen is already there
+    n, j=r, x#assign new values and start from the biggest possible row/column
+    #this one we check if is diagonal from the right
+    for z in range(n,0,-1): #rows 
+        for y in range(j,0,-1):#columns 
+            if
 
-def place(grid):
+            
+# to iterate by row set r
+def place(grid, r):
     #the place function needs to check if its possible then return the value
-    for x in range(r):
-       if if_possible(grid, x):
+    #first we iterate the column with x
+    for i in range(N):
+       if is_possible(board, r, i):
+           grid[r][i]='Q'
+           place(grid, r+1)
+
+        #and here we use this to iterate the row using recursion
+        
+    
+
+#start r at zero
+
+place(board, 0)
